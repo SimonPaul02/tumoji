833bf74989505ae41091391c1fe5936fb77cd934
SimonPaul02 <Simonpaul987123@gmail.com>
2021-10-15 11:46:04 +0200

    ;)

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import os
 import nltk
-import nltk.corpus
+from nltk import FreqDist
 
 
 def main():
-    print("tumoji ;)")
+    lyrics = '''O! say can you see
+by the dawn’s early light,
+What so proudly we hailed
+at the twilight’s last gleaming,
+Whose broad stripes and bright stars
+through the perilous fight,
+O’er the ramparts we watched,
+were so gallantly streaming?
+And the rockets’ red glare,
+the bombs bursting in air,
+Gave proof through the night
+that our flag was still there;
+O! say does that star-spangled
+banner yet wave,
+O’er the land of the free
+and the home of the brave?'''
+    tokens = nltk.word_tokenize(lyrics)
+    fdist = FreqDist(tokens)
+    print(fdist.keys())
+    print(fdist.values())
 
 
 if __name__ == '__main__':
