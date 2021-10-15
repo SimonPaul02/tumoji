5ac0af50d0afc9b43bdda2d670b98f5ee98138bf
Parxeval <thepembasherpa@gmail.com>
2021-10-15 12:56:09 +0200

    Removed Stop Words

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import nltk
 from nltk import FreqDist
+from nltk.sentiment import SentimentIntensityAnalyzer
 
+#Sentiment Analysis
+def is_positive(lyrics: str) -> bool:
+    return SentimentIntensityAnalyzer.polarity_scores(lyrics)["compound"] > 0
+    
+#Main
 def main():
     lyrics = '''O! say can you see
 by the dawn’s early light,
@@ -18,6 +24,7 @@ O! say does that star-spangled
 banner yet wave,
 O’er the land of the free
 and the home of the brave?'''
+    
     stopwords = nltk.corpus.stopwords.words("english")
     tokens = nltk.word_tokenize(lyrics)
     lyricsWithoutSW = [w for w in tokens if not w.lower() in stopwords]
@@ -25,7 +32,7 @@ and the home of the brave?'''
     fdist = FreqDist(lyricsWithoutSW)
     print(fdist.keys())
     print(fdist.values())
-
+    fdist.tabulate(5)
 
 if __name__ == '__main__':
     main()
