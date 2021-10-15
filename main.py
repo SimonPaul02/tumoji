b4ac204c54d78edb0780aad9da14ac8d0477aa5b
SimonPaul02 <Simonpaul987123@gmail.com>
2021-10-15 13:24:35 +0200

    changed prints

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import nltk
 from nltk import FreqDist
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-#Sentiment Analysis
+
+# Sentiment Analysis
 def is_positive(lyrics: str) -> bool:
     return SentimentIntensityAnalyzer.polarity_scores(lyrics)["compound"] > 0
-    
-#Main
+
+
+# Main
 def main():
     lyrics = '''O! say can you see
 by the dawn’s early light,
@@ -24,15 +26,21 @@ O! say does that star-spangled
 banner yet wave,
 O’er the land of the free
 and the home of the brave?'''
-    
+
     stopwords = nltk.corpus.stopwords.words("english")
     tokens = nltk.word_tokenize(lyrics)
+    print("tokens: ")
+    print(tokens)
     lyricsWithoutSW = [w for w in tokens if not w.lower() in stopwords]
+    print("lyrics Without stop words: ")
     print(lyricsWithoutSW)
     fdist = FreqDist(lyricsWithoutSW)
+    print("How many times each word")
     print(fdist.keys())
-    print(fdist.values())
-    fdist.tabulate(5)
+    print( fdist.values())
+    print(" most words")
+    print(fdist.tabulate(10))
+
 
 if __name__ == '__main__':
     main()
